445401a5726fa2d65de8835020b9bdc5d20496ab
jwCheng28 <chengjw68@gmail.com>
2026-06-20 18:59:20 -0700

    fix bug: missed padding

--- papers/resnet/src/model.py
+++ papers/resnet/src/model.py
@@ -14,7 +14,7 @@ class Bottleneck(nn.Module):
         self.bn1 = nn.BatchNorm2d(out_channels)
 
         self.conv2 = nn.Conv2d(in_channels, out_channels,
-                               kernel_size=3, stride=stride, bias=False)
+                               kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.conv3 = nn.Conv2d(
