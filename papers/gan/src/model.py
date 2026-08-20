520d6d1af626972a269043055f09a7aee0a82f61
jwCheng28 <chengjw68@gmail.com>
2026-08-19 23:58:25 -0700

    fix typo

--- papers/gan/src/model.py
+++ papers/gan/src/model.py
@@ -34,7 +34,7 @@ class Generator(nn.Module):
 
             nn.ConvTranspose2d(64, 1, kernel_size=4,
                                stride=2, padding=1, bias=False),
-            nn.Tanh(True)
+            nn.Tanh()
         )
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
